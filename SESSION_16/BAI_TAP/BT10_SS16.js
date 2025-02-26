4ff2a37dc61396c621d42e0c78063f7c79fa6484
ThanhIT2006 <nguyenngocthanh15122006@gmail.com>
2025-02-26 17:13:44 +0700

    BTVN

--- SESSION_16/BAI_TAP/BT10_SS16.js
+++ SESSION_16/BAI_TAP/BT10_SS16.js
@@ -1,4 +1,4 @@
 let firstNumber=Number(prompt("Nhập một số bất kì thứ nhất: "));
 let secondNumber=Number(prompt("Nhập một số bất kì thứ hai: "));
-let numberRandom=Math.floor(Math.random(firstNumber)* secondNumber);
+let numberRandom=Math.floor(Math.random()* (secondNumber - firstNumber))+firstNumber;
 document.write("Số bất kì trong khoảng hai số vừa nhập là: ",numberRandom);
