751b657ef726373deb733f19fb25f2e833f74be8
ThanhIT2006 <nguyenngocthanh15122006@gmail.com>
2025-02-27 17:17:37 +0700

    BTVN

--- SESSION_17/BAI_TAP/BT13_SS17.js
+++ SESSION_17/BAI_TAP/BT13_SS17.js
@@ -1,4 +1,4 @@
 let money=Number(prompt("Nhập số tiền bạn muốn gửi: "));
 let month=Number(prompt("Nhập số tháng bạn muốn gửi: "));
 let result=money*4.3/100*month/12;
-document.write=("Số tiền lãi của bạn là: "+result);
+console.log("Số tiền lãi của bạn là: "+result);
