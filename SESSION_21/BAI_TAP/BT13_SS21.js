52228dfe8726436c6130e7a5d200b169be6173ab
ThanhIT2006 <nguyenngocthanh15122006@gmail.com>
2025-03-06 01:31:41 +0700

    BTVN

--- SESSION_21/BAI_TAP/BT13_SS21.js
+++ SESSION_21/BAI_TAP/BT13_SS21.js
@@ -1,44 +1,53 @@
 document.write("a.<br>");
-for(let i=0;i<10;i++){
-    for (let j = 10; j >0+i ; j--) {
-       document.write("*");
-
+for (let i = 0; i < 10; i++) 
+{
+    for (let j = 10; j > 0 + i; j--) 
+    {
+        document.write("*");
     }
     document.write("<br>");
 }
 document.write("<br>");
 document.write("b.<br>");
-for(let i=10;i>0;i--){
-    for (let j = 11; j >i ; j--) {
-       document.write("*");
+for (let i = 10; i > 0; i--) 
+{
+    for (let j = 11; j > i; j--) 
+    {
+        document.write("*");
     }
     document.write("<br>");
 }
 document.write("<br>");
 document.write("c.<br>");
-for(let i=1;i<11;i++){
-    for (let j = 11; j >0 ; j--) {
-       if (j>i) {
-        document.write(" &nbsp;");
-       }else{
-    
-        document.write("*");
-       }
-       
+for (let i = 1; i < 11; i++) 
+{
+    for (let j = 11; j > 0; j--) 
+    {
+        if (j > i) 
+        {
+            document.write(" &nbsp;");
+        }
+        else 
+        {
+            document.write("*");
+        }
     }
     document.write("<br>");
 }
 document.write("<br>");
 document.write("d.<br>");
-for(let i=11;i>0;i--){
-    for (let j = 11; j >0 ; j--) {
-       if (j>i) {
-        document.write(" &nbsp;");
-       }else{
-    
-        document.write("*");
-       }
-       
+for (let i = 11; i > 0; i--) 
+{
+    for (let j = 11; j > 0; j--) 
+    {
+        if (j > i) 
+        {
+            document.write(" &nbsp;");
+        } 
+        else 
+        {
+            document.write("*");
+        }
     }
     document.write("<br>");
 }
